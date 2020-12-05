c47479de423323ce31fdc8096f36d10ff76c1358
Sektor <krzysiek1230@gmail.com>
2020-12-05 15:25:42 +0100

    Add axios and authSlice

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -1,22 +1,81 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import Axios, { AxiosError, AxiosResponse } from "axios";
+import { BaseResponse } from "../../types/BaseResponse";
+import { API_LOGIN_USER, API_REGISTER_USER } from "../../utils/api.constants";
 
 interface InitialState {
-  userName: string;
-  password: string;
+  user: {
+    userName: string;
+    token: string;
+  } | null;
   errorMsg?: string;
 }
 
-const initialState = { userName: "" };
+const initialState: InitialState = { user: null };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {
-    loginUser(state, action: PayloadAction<{}>) {},
-    registerUser(state, action: PayloadAction<{}>) {},
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.errorMsg = undefined;
+      state.user = {
+        token: action.payload.accessToken!,
+        userName: action.payload.userName!,
+      };
+    });
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.errorMsg = action.payload!.loginError;
+      state.user = null;
+    });
   },
 });
 
-export const { loginUser, registerUser } = authSlice.actions;
+export const registerUser = async (
+  userName: string,
+  password: string
+): Promise<BaseResponse> => {
+  try {
+    await Axios.post(API_REGISTER_USER, { userName, password });
+    return { succes: true };
+  } catch (e) {
+    return { succes: false, errorMsg: e.message };
+  }
+};
+
+type LoginFullfilledResponse = {
+  userName: string;
+  accessToken: string;
+};
+type LoginPayload = {
+  userName: string;
+  password: string;
+};
+type LoginRejectedResponse = {
+  loginError?: string;
+};
+
+export const loginUser = createAsyncThunk<
+  LoginFullfilledResponse,
+  LoginPayload,
+  { rejectValue: LoginRejectedResponse }
+>("auth/loginUser", async (payload, { rejectWithValue }) => {
+  const res: AxiosResponse<LoginFullfilledResponse> = await Axios.post(
+    API_LOGIN_USER,
+    {
+      ...payload,
+    }
+  );
+  try {
+    return res.data;
+  } catch (e) {
+    const error: AxiosError<LoginRejectedResponse> = e;
+    if (!error.response) {
+      throw e;
+    }
+    return rejectWithValue(error.response.data);
+  }
+});
 
 export default authSlice.reducer;
---
+++ src/utils/api.constants.ts
@@ -0,0 +1,4 @@
+const API_BASE_URL = "http://localhost:8000";
+
+export const API_REGISTER_USER = `${API_BASE_URL}/register`;
+export const API_LOGIN_USER = `${API_BASE_URL}/login`;
